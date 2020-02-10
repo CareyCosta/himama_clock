e29bc0af8564d30bbafce7da35f10ed02d11afb9
Carey Costa <carey@loopio.com>
2020-02-09 22:37:50 -0500

    clean up routing

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,16 +1,10 @@
 import React from "react";
-import Signup from "./registrations/Signup";
 import Home from "./Home";
 
-const Welcome = ({ loggedInStatus, onLogin, history, user }) => {
-  return (
-    <div>
-      {!loggedInStatus ? (
-        <Signup onLogin={onLogin} history={history} />
-      ) : (
-        <Home user={user} />
-      )}
-    </div>
-  );
+const Welcome = ({ loggedInStatus, history, user }) => {
+  if (!loggedInStatus) {
+    history.push("/signup");
+  }
+  return <Home user={user} />;
 };
 export default Welcome;
